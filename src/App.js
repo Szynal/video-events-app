677f8d18c05fe1dadface3cd4ff92af231852492
Paweł Szynal <pawel@szynal.info>
2021-03-10 03:37:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          PWR
+          video-conferencing-app under development
         </p>
       </header>
     </div>

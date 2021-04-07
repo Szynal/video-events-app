ead1c6a0bfaa9d6a62ffcfbe68753ff1c39f792d
Paweł Szynal <pawel@szynal.info>
2021-04-07 21:35:56 +0200

    Demo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 // Router //
 import { Router, Switch, Route } from 'react-router-dom';
-// import history from 'utils/history';
+import history from 'utils/history';
 
 // Styles //
 import { ThemeProvider } from 'styled-components';
 import theme from 'styles/theme';
 import GlobalStyles from 'styles/global';
-import '@voxeet/react-components/dist/voxeet-react-components.css';
+import '@voxeet/react-components/dist/voxeet-react-components.css'; // Can you be customize, refer to https://github.com/voxeet/voxeet-assets-react-components
 
+// Screens //
+import Conference from 'screens/Conference';
+import Login from 'screens/Login';
+
+// Containers //
+import AuthedRoute from 'containers/AuthedRoute';
+import Snackbar from 'containers/Snackbar';
 
 // Redux //
-// import { Provider } from 'react-redux';
-// import createStore from 'data/createStore';
+import { Provider } from 'react-redux';
+import createStore from 'data/createStore';
+
+const store = createStore();
 
 function App() {
-  return (
-    
-  <ThemeProvider theme={theme}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-  </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <Router history={history}>
+                    <>
+                        <Switch>
+                            <AuthedRoute path='/:conferenceAlias' component={Conference} />
+                            <Route path='/' component={Login} />
+                        </Switch>
+                        <GlobalStyles />
+                        <Snackbar />
+                    </>
+                </Router>
+            </Provider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
